84036579c2867601ee97a752f6bed5f26392eb26
josh <joshmuente@gmail.com>
2024-04-08 03:34:19 +0200

    refactor main and better output

--- src/strategy.rs
+++ src/strategy.rs
@@ -1,37 +1,44 @@
 use crate::{
     bindings::{
-        clearinghouse::Clearinghouse,
-        cooler::{self, Cooler},
+        clearinghouse::{ClaimDefaultedCall, Clearinghouse},
+        cooler::Cooler,
         cooler_factory::{
             ClearRequestFilter, CoolerFactory, DefaultLoanFilter, ExtendLoanFilter, RepayLoanFilter,
         },
     },
-    utils::{get_sys_time_in_secs, get_token_price},
+    utils::{get_sys_time_in_secs, get_token_price, greet},
 };
 use anyhow::Result;
 use artemis_core::{executors::mempool_executor::SubmitTxToMempool, types::Strategy};
 use async_trait::async_trait;
-use ethers::{
-    contract::parse_log,
-    providers::Middleware,
-    types::{Address, U256},
-};
+use comfy_table::{presets::UTF8_FULL, Attribute, Cell, Color, Table};
+use ethers::{contract::parse_log, providers::Middleware, types::U256};
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use std::{fmt::Write, sync::Arc};
 
 use crate::types::{Action, Event};
 
+use chrono::{DateTime, TimeZone, Utc};
+
 #[derive(Debug, Clone)]
 pub struct LoanTarget<M> {
-    pub cooler: cooler::Cooler<M>,
-    pub req_id: ::ethers::core::types::U256,
-    pub loan_id: ::ethers::core::types::U256,
-    pub collateral: ::ethers::core::types::U256,
-    pub expiry: ::ethers::core::types::U256,
+    pub cooler: Cooler<M>,
+    pub req_id: U256,
+    pub loan_id: U256,
+    pub collateral: U256,
+    pub expiry: U256,
+}
+
+#[derive(Debug)]
+pub struct LiquidationStrategy<M> {
+    pub client: Arc<M>,
+    pub clearinghouse: Clearinghouse<M>,
+    pub cooler_factory: CoolerFactory<M>,
+    pub loans: Vec<LoanTarget<M>>,
 }
 
 impl<M: Middleware + 'static> LoanTarget<M> {
-    pub async fn new(cooler: cooler::Cooler<M>, req_id: U256, loan_id: U256) -> Self {
+    pub async fn new(cooler: Cooler<M>, req_id: U256, loan_id: U256) -> Self {
         let loan = cooler.get_loan(loan_id).await.unwrap();
         Self {
             cooler,
@@ -48,7 +55,7 @@ impl<M: Middleware + 'static> LoanTarget<M> {
         self.expiry = loan.expiry;
     }
 
-    pub async fn is_claimable(&self, timestamp: U256) -> bool {
+    pub fn is_claimable(&self, timestamp: U256) -> bool {
         if self.expiry < timestamp && self.collateral > 0.into() {
             return true;
         } else {
@@ -56,7 +63,20 @@ impl<M: Middleware + 'static> LoanTarget<M> {
         }
     }
 
-    pub async fn calc_rewards(&self, timestamp: U256, ohm_price: U256) -> U256 {
+    pub fn calc_reward_percentage(&self) -> U256 {
+        let timestamp = U256::from(get_sys_time_in_secs());
+        let elapsed = timestamp - self.expiry;
+        let seven_days_in_s: U256 = (7 * 24 * 60 * 60).into();
+        let reward_percentage = if elapsed < seven_days_in_s {
+            (elapsed * 100) / seven_days_in_s
+        } else {
+            100.into()
+        };
+
+        return reward_percentage;
+    }
+
+    pub fn calc_rewards_in_dollar(&self, timestamp: U256, ohm_price: U256) -> U256 {
         let elapsed = timestamp - self.expiry;
         let seven_days_in_s: U256 = (7 * 24 * 60 * 60).into();
         let mut max_reward: U256 = (1e17 as u64).into();
@@ -68,25 +88,16 @@ impl<M: Middleware + 'static> LoanTarget<M> {
             max_reward
         };
 
-        let mut reward_in_gohm: U256 = 0.into();
-        if elapsed < seven_days_in_s {
-            reward_in_gohm = (max_reward * elapsed) / seven_days_in_s;
+        let reward_in_gohm: U256 = if elapsed < seven_days_in_s {
+            (max_reward * elapsed) / seven_days_in_s
         } else {
-            reward_in_gohm = max_reward;
-        }
+            max_reward
+        };
 
         let reward_in_dollar = reward_in_gohm * ohm_price / (1e18 as u64);
 
         return reward_in_dollar.into();
     }
-}
-
-#[derive(Debug, Clone)]
-pub struct LiquidationStrategy<M> {
-    pub client: Arc<M>,
-    pub clearinghouse: Clearinghouse<M>,
-    pub cooler_factory: CoolerFactory<M>,
-    pub loans: Vec<LoanTarget<M>>,
 }
 
 impl<M: Middleware + 'static> LiquidationStrategy<M> {
@@ -105,6 +116,145 @@ impl<M: Middleware + 'static> LiquidationStrategy<M> {
 }
 
 impl<M: Middleware + 'static> LiquidationStrategy<M> {
+    async fn print_table(&self, claimable: U256, gohm_price: U256) {
+        println!("\x1B[2J\x1B[1;1H");
+        greet();
+
+        let mut table_info = Table::new();
+        let ohm_price = get_token_price("governance-ohm").await.unwrap() as u64;
+        let expired_loans: Vec<&LoanTarget<M>> = self
+            .loans
+            .iter()
+            .filter(|loan| {
+                loan.expiry < U256::from(get_sys_time_in_secs())
+                    && loan.collateral > 0.into()
+                    && loan.calc_rewards_in_dollar(
+                        U256::from(get_sys_time_in_secs()),
+                        ohm_price.into(),
+                    ) > 0.into()
+            })
+            .collect();
+
+        let total_collateral_gohm = expired_loans
+            .iter()
+            .fold(U256::from(0), |acc, loan| acc + loan.collateral)
+            / (1e18 as u64) as u64;
+
+        let timestamp = U256::from(get_sys_time_in_secs());
+        let next_expiry = self.loans.iter().fold(U256::MAX, |acc, loan| {
+            if loan.expiry > timestamp && loan.expiry < acc {
+                loan.expiry - timestamp
+            } else {
+                acc
+            }
+        });
+
+        let claiable_consider_gas_and_targets =
+            self.loans.iter().filter(|loan| {
+                loan.is_claimable(U256::from(get_sys_time_in_secs()))
+                    && loan.calc_rewards_in_dollar(
+                        U256::from(get_sys_time_in_secs()),
+                        ohm_price.into(),
+                    ) > 0.into()
+            })
+            .fold(U256::from(0), |acc, loan| {
+                if loan.calc_reward_percentage()
+                    > std::env::var("REWARD_PERIOD_TARGET")
+                        .unwrap()
+                        .parse()
+                        .unwrap()
+                {
+                    return acc + loan.calc_rewards_in_dollar(
+                        U256::from(get_sys_time_in_secs()),
+                        ohm_price.into(),
+                    )
+                } else {
+                    return acc
+                }
+            });
+
+        let claiable_consider_gas_and_targets: U256 =
+            if claiable_consider_gas_and_targets < std::env::var("MIN_PROFIT").unwrap().parse().unwrap() {
+                0.into()
+            } else {
+                claiable_consider_gas_and_targets.into()
+            };
+
+        table_info.load_preset(UTF8_FULL).set_header(vec![
+            "Claimable",
+            "Claimable inc. gas and target",
+            "Profit Target",
+            "Reward Period Target",
+            "Expired Loans",
+            "Total Collateral",
+            "Next Expiry",
+        ]);
+
+        let duration: DateTime<Utc> = Utc.timestamp_opt(next_expiry.as_u64() as i64, 0).unwrap();
+        let duration = duration.format("%Hh:%Mm:%Ss");
+        table_info.load_preset(UTF8_FULL).add_row(vec![
+            format!("{} dollar", claimable.to_string()),
+            format!("{} dollar", claiable_consider_gas_and_targets.to_string()),
+            format!("{} dollar", std::env::var("MIN_PROFIT").unwrap()),
+            format!("{}%", std::env::var("REWARD_PERIOD_TARGET").unwrap()),
+            expired_loans.len().to_string(),
+            format!("{} gOHM", total_collateral_gohm.to_string()),
+            format!("{}", duration),
+        ]);
+
+        let mut table_loans = Table::new();
+        table_loans.load_preset(UTF8_FULL).set_header(vec![
+            "Cooler",
+            "Loan ID",
+            "Collateral",
+            "Expire time (UTC)",
+            "Reward period passed",
+            "Reward",
+        ]);
+        for loan in expired_loans.iter() {
+            let is_reward_period_target_hit = loan.calc_reward_percentage()
+                > std::env::var("REWARD_PERIOD_TARGET")
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+            let reward_target_text = format!("{}%", loan.calc_reward_percentage());
+            let reward_target_text: Cell = if is_reward_period_target_hit {
+                Cell::new(reward_target_text)
+                    .fg(Color::Green)
+                    .add_attributes(vec![Attribute::Bold])
+            } else {
+                Cell::new(reward_target_text)
+            };
+
+            let readable_expiry = chrono::Utc
+                .timestamp_opt(loan.expiry.as_u64() as i64, 0)
+                .unwrap();
+            let readable_expiry = readable_expiry.format("%Y-%m-%d %H:%M:%S").to_string();
+            table_loans.load_preset(UTF8_FULL).add_row(vec![
+                Cell::new(loan.cooler.address().to_string()),
+                Cell::new(loan.loan_id.to_string()),
+                Cell::new(loan.collateral.to_string()),
+                Cell::new(readable_expiry),
+                reward_target_text,
+                Cell::new(format!(
+                    "{} dollar",
+                    loan.calc_rewards_in_dollar(
+                        U256::from(get_sys_time_in_secs()),
+                        gohm_price.into(),
+                    )
+                    .to_string(),
+                )),
+            ]);
+        }
+
+        println!();
+        println!("{}", table_info);
+
+        if expired_loans.len() > 0 {
+            println!();
+            println!("{}", table_loans);
+        }
+    }
     pub async fn set_loans(&mut self) -> Result<()> {
         println!("Fetching Cooler Loans... ");
         let event: ethers::contract::Event<_, _, _> = self.cooler_factory.clear_request_filter();
@@ -124,7 +274,7 @@ impl<M: Middleware + 'static> LiquidationStrategy<M> {
                 .progress_chars("#>-"),
         );
         for log in logs.iter() {
-            let cooler = cooler::Cooler::new(log.cooler, self.client.clone());
+            let cooler = Cooler::new(log.cooler, self.client.clone());
             let new_loan = LoanTarget::new(cooler, log.req_id, log.loan_id).await;
 
             self.loans.push(new_loan);
@@ -150,62 +300,96 @@ impl<M: Middleware + 'static> Strategy<Event, Action> for LiquidationStrategy<M>
     async fn process_event(&mut self, event: Event) -> Vec<Action> {
         match event {
             Event::NewBlock(_) => {
-                let timestamp = U256::from(get_sys_time_in_secs());
-                let ohm_price: U256 =
-                    (get_token_price("governance-ohm").await.unwrap() as u64).into();
+                let gohm_price = get_token_price("governance-ohm").await.unwrap() as u64;
+                let mut claimable_loans = self
+                    .loans
+                    .iter_mut()
+                    .filter(|loan| {
+                        loan.is_claimable(U256::from(get_sys_time_in_secs()))
+                            && loan.calc_rewards_in_dollar(
+                                U256::from(get_sys_time_in_secs()),
+                                gohm_price.into(),
+                            ) > 0.into()
+                    })
+                    .collect::<Vec<&mut LoanTarget<M>>>();
 
-                let mut total_rewards: U256 = 0.into();
-                let mut claimable: (Vec<Address>, Vec<U256>) = (vec![], vec![]);
-                for loan in self.loans.iter_mut() {
-                    if loan.is_claimable(timestamp).await {
-                        loan.update().await;
-                        let reward = loan.calc_rewards(timestamp, ohm_price).await;
-                        if reward > 0.into() {
-                            claimable.0.push(loan.cooler.address());
-                            claimable.1.push(loan.loan_id);
-                        }
-                        total_rewards += reward;
-                    }
+                let claimable_dollar_raw =
+                    claimable_loans.iter_mut().fold(U256::from(0), |acc, loan| {
+                        acc + loan.calc_rewards_in_dollar(
+                            U256::from(get_sys_time_in_secs()),
+                            gohm_price.into(),
+                        )
+                    });
+
+                let mut claimable_loans_with_reward_limit_hit = claimable_loans
+                    .iter_mut()
+                    .filter(|loan| {
+                        loan.calc_reward_percentage()
+                            > std::env::var("REWARD_PERIOD_TARGET")
+                                .unwrap()
+                                .parse()
+                                .unwrap()
+                    })
+                    .collect::<Vec<&mut &mut LoanTarget<M>>>();
+
+                for loan in claimable_loans_with_reward_limit_hit.iter_mut() {
+                    loan.update().await;
                 }
 
-                if total_rewards <= 0.into() {
+                if claimable_loans_with_reward_limit_hit.len() == 0 {
+                    self.print_table(claimable_dollar_raw, gohm_price.into()).await;
                     return vec![];
                 }
 
+                let claimable_reward_hit_dollar = claimable_loans_with_reward_limit_hit
+                    .iter()
+                    .fold(U256::from(0), |acc, loan| {
+                        acc + loan.calc_rewards_in_dollar(
+                            U256::from(get_sys_time_in_secs()),
+                            gohm_price.into(),
+                        )
+                    });
+
+                let claim_default_arguments: ClaimDefaultedCall =
+                    claimable_loans_with_reward_limit_hit.iter().fold(
+                        ClaimDefaultedCall {
+                            loans: vec![],
+                            coolers: vec![],
+                        },
+                        |mut acc, loan| {
+                            acc.loans.push(loan.loan_id);
+                            acc.coolers.push(loan.cooler.address());
+                            acc
+                        },
+                    );
+
+                let gas_price = self.client.get_gas_price().await.unwrap();
                 let tx = self
                     .clearinghouse
-                    .claim_defaulted(claimable.clone().0, claimable.clone().1)
+                    .claim_defaulted(
+                        claim_default_arguments.coolers,
+                        claim_default_arguments.loans,
+                    )
                     .tx;
+
                 let gas_estimate = self.client.estimate_gas(&tx, None).await.unwrap();
-                let gas_price = self.client.get_gas_price().await.unwrap();
-                let eth_price = get_token_price("ethereum").await.unwrap() as u64;
-                let gas_cost_dollar = gas_estimate * gas_price * eth_price / (1e+18 as u64);
-                let min_profit: U256 = std::env::var("MIN_PROFIT").unwrap().parse().unwrap();
+                let gas_cost = gas_estimate * gas_price;
+                let gas_cost_dollar =
+                    gas_cost * get_token_price("ethereum").await.unwrap() as u64 / 1e18 as u64;
+                let net_claimable_reward_target_hit_dollar =
+                    claimable_reward_hit_dollar - gas_cost_dollar;
+                let profit_target_hit = net_claimable_reward_target_hit_dollar
+                    > std::env::var("MIN_PROFIT").unwrap().parse().unwrap();
 
-                if total_rewards > 0.into() {
-                    println!(
-                        "Found a total of {} claimable cooler loans",
-                        claimable.0.len()
-                    );
-                    println!(
-                        "Rewards {}, Gas Cost {}, Min Profit {}",
-                        total_rewards.to_string().parse::<f64>().unwrap(),
-                        gas_cost_dollar.to_string().parse::<f64>().unwrap(),
-                        min_profit.to_string().parse::<f64>().unwrap()
-                    );
-                    println!("{:?}\n", claimable);
-                }
+                self.print_table(claimable_dollar_raw, gohm_price.into()).await;
 
-                if total_rewards > gas_cost_dollar && (total_rewards - gas_cost_dollar) > min_profit
-                {
-                    println!("Claiming rewards for {} loans", claimable.0.len());
+                if profit_target_hit {
+                    println!("[ACTION] Claiming loans...");
                     return vec![Action::SubmitTx(SubmitTxToMempool {
                         tx,
                         gas_bid_info: None,
                     })];
                 }
-
-                return vec![];
             }
 
             Event::NewLoan(log) => {
